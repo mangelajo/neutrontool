9da852dd8322968e82c856ec30f771dac8385d10
Miguel Angel Ajo <mangelajo@redhat.com>
2014-10-21 18:53:36 +0200

    Show equivalent remote CIDRs

--- neutrontool/neutrontool.py
+++ neutrontool/neutrontool.py
@@ -25,9 +25,11 @@ class SecurityGroupScaleHealer():
         self._sgs = {}
         self._ports = {}
         self._tenants = {}
+        self._subnets = {}
         self._fetch_tenant_list()
         self._fetch_security_groups()
         self._fetch_ports()
+        self._fetch_subnets()
 
     def _fetch_tenant_list(self):
 
@@ -62,6 +64,26 @@ class SecurityGroupScaleHealer():
             security_group.ports = {}
             self._sgs[security_group.id] = security_group
 
+    def _fetch_subnets(self):
+        print color('blue',"Fetching subnet listing ....")
+        subnets = self.neutron.list_subnets()['subnets']
+        for subnet in subnets:
+            self._subnets[subnet['id']] = AttrDict(subnet)
+
+    def _get_sg(self, sg_id):
+        if sg_id not in self._sgs:
+            # retrieve any missing SG (from other tenants)
+            print color('red', sg_id)
+            self._sgs[sg_id] = self._attrdict_sg(
+                self.neutron.show_security_group(sg_id))
+        return self._sgs.get(sg_id, None)
+
+    def _get_subnet(self, subnet_id):
+        if subnet_id not in self._subnets:
+            subnet = self.neutron.show_subnet(subnet_id)['subnet']
+            self._subnets[subnet_id] = AttrDict(subnet)
+ 
+        return self._subnets.get(subnet_id, None)
 
     def _fetch_ports(self):
         print color('blue', "Fetching port information ....")
@@ -69,10 +91,6 @@ class SecurityGroupScaleHealer():
         for port in ports:
             self._ports[port.id] = port
             for sg_id in port.security_groups:
-                if sg_id not in self._sgs:
-                    # retrieve any missing SG (from other tenants)
-                    self_sgs[sg_id] = self._attrdict_sg(
-                        self.neutron.show_security_group(sg_id))
                 self._sgs[sg_id].ports[port.id] = port
 
     def _iter_rules_with_remote_gid(self, rules):
@@ -101,14 +119,45 @@ class SecurityGroupScaleHealer():
 
             bad_sg.tenant_name = (tenant.name if tenant
                                   else "<unknown: %s>" % bad_sg.tenant_id)
+
             print color('header',"\nsecurity_group %(id)s (%(name)s) has remote "
                                  "gid rules %(used_str)s\n  tenant: "
                                  "%(tenant_name)s" % bad_sg)
+
             for port_id, port in bad_sg.ports.iteritems():
                 ip_addresses = ", ".join([ip['ip_address'] for ip in port.fixed_ips])
-                subnets = [ip['subnet_id'] for ip in port.fixed_ips]
                 port_info = {'id': port.id, 'ips': ip_addresses}
                 print ("\tport %(id)s with ips: %(ips)s" % port_info)
+
+            self._report_equivalent_subnet_CIDRs(bad_sg)
+
+    def _report_equivalent_subnet_CIDRs(self, sg):
+        subnets = self._get_remote_subnets_from_remote_gid_rules(sg)
+        print color('warning',"  Equivalent remote subnet CIDRs:")
+        for __, subnet in subnets.iteritems():
+            print color('warning',"    subnet: %(name)s\tCIDR: %(cidr)s" % subnet)
+
+    def _get_remote_subnets_from_remote_gid_rules(self, sg):
+        rules = self._iter_rules_with_remote_gid(sg.security_group_rules)
+        remote_ports = self._get_remote_ports_from_sg_rules(rules)
+        return self._get_ports_subnets(remote_ports)
+
+    def _get_ports_subnets(self, ports):
+        subnets = {}
+        for __, port in ports.iteritems():
+            for ip in port.fixed_ips:
+                subnets[ip['subnet_id']] = self._get_subnet(ip['subnet_id'])
+        return subnets
+
+    def _get_remote_ports_from_sg_rules(self, rules):
+        remote_ports = {}
+        for rule in rules:
+            sg_rg_id = rule.remote_group_id
+            remote_sg = self._get_sg(sg_rg_id)
+            for port_id, port in remote_sg.ports.iteritems():
+                remote_ports[port_id] = port
+        return remote_ports
+
 
 def build_parser():
     def extra_help():
@@ -130,7 +179,6 @@ def main():
     options = build_parser().parse_args()
     neutron = osclients.create_neutron_client(options)
     keystone = osclients.create_keystone_client(options)
-    print options.command
     if options.command == 'report':
         healer = SecurityGroupScaleHealer(neutron, keystone)
         healer.report()
